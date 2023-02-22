e00308dc979c8b4b001b44b1e1f833bb1160b0ba
Idowufaith-data <idowufaith4u@gmail.com>
2023-02-22 03:59:54 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,7 @@
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
@@ -11,9 +12,9 @@ void print_alphabet_x10(void)
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
-{			
+	{			
 	_putchar(c);
-}
+	}
 	_putchar('\n');
 	i++;
 	}
