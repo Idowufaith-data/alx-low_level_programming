5d593f461dc75cad95bd0fbff34c76e44e1d69be
Idowufaith-data <idowufaith4u@gmail.com>
2023-03-16 08:20:50 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - Prints out string on the screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
