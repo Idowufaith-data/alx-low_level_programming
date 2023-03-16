0d60cc6eeda0991f3a243b09ee4333d6e99395f9
Idowufaith-data <idowufaith4u@gmail.com>
2023-03-16 08:00:43 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Puts()-Writes a string to the output
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
