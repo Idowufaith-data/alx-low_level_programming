9f8a75b5f4339e577e29eaae7dade7946cbfd3a4
Idowufaith-data <idowufaith4u@gmail.com>
2023-03-23 05:37:37 -0700

    Isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int prime_number(long int n);
+long int biggest_factor(long int a);
+void print_number(int n);
+int num_digits(int n);
+#endif
