a9d8d76f75d320ac7f425887db8e4fc6a22293e1
Idowufaith-data <idowufaith4u@gmail.com>
2023-03-27 17:17:54 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - holds the pointers pointing to variables a and b
+ *
+ * @a: parameter
+ * @b: parameter
+ *
+ * Description - a function that swaps the values of two integers
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
