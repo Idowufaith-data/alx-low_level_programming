5d03b98921ef830998e745f8adcba3b06d0a5299
Idowufaith-data <idowufaith4u@gmail.com>
2023-03-27 17:09:04 -0700

    98 Battery str

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes an int to a pointer
+ *
+ * @n: parameter
+ * Description - A function that takes pointer to an int and updates
+ * the value it points to
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
